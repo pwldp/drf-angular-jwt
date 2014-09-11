f1140a17debf80f0ce6d16949a7aff943d89b1d6
reklak <reklak@gmail.com>
2014-09-11 15:45:39 +0200

    repaired /users/ path

--- application/application/urls.py
+++ application/application/urls.py
@@ -34,6 +34,7 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns+= patterns(
+    '',
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
